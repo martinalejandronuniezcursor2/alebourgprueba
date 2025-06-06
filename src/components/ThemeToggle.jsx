7854804afe7c93296208502bd32f1c6ba77fc025
Martin Nuñez <martinalejandronuniez@gmail.com>
2025-06-06 00:05:09 -0300

    mejora buena

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { FaSun, FaMoon } from 'react-icons/fa';
 
 const ThemeToggle = () => {
-  const [isDark, setIsDark] = useState(true);
+  const [isDark, setIsDark] = useState(false);
 
   useEffect(() => {
     // Aplicar tema inicial
